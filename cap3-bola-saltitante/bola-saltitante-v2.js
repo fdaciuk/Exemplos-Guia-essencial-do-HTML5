205631aecd9c920577f8679b9ed1d15d19d1e74d
Fernando Daciuk <f.daciuk@gmail.com>
2013-01-17 13:28:40 -0200

    Bola Saltitante - V2

--- cap3-bola-saltitante/bola-saltitante-v2.js
+++ cap3-bola-saltitante/bola-saltitante-v2.js
@@ -2,7 +2,7 @@ var boxx = 20,
 	boxy = 30,
 	boxwidth = 350,
 	boxheight = 250,
-	ballrad = 10,
+	ballrad = 20,
 	boxboundx = boxwidth + boxx - ballrad,
 	boxboundy = boxheight + boxy - ballrad,
 	inboxboundx = boxx + ballrad,
@@ -10,13 +10,38 @@ var boxx = 20,
 	ballx = 50,
 	bally = 60,
 	ctx,
+	grad,
+	color,
+	hue = [],
 	ballvx = 4,
-	ballvy = 8;
+	ballvy = 8,
+
+	img = new Image();
+
+img.src = "frogface.gif";
+
+hue = [
+	[ 255,   0,   0 ],
+	[ 255, 255,   0 ],
+	[   0, 255,   0 ],
+	[   0, 255, 255 ],
+	[   0,   0, 255 ],
+	[ 255,   0, 255 ]
+];
 
 function init() {
+	var h;
 	ctx = document.getElementById( 'canvas' ).getContext( '2d' );
+
+	grad = ctx.createLinearGradient( boxx, boxy, boxx + boxwidth, boxy + boxheight );
+
+	for( h = 0; h < hue.length; h++ ) {
+		color = 'rgb( ' + hue[h][0] + ', ' + hue[h][1] + ', ' + hue[h][2] + ' )';
+		grad.addColorStop( h * 1/6, color );
+	}
+
+	ctx.fillStyle = grad;
 	ctx.lineWidth = ballrad;
-	ctx.fillStyle = "rgb( 200, 0, 50 )";
 	moveball();
 
 	setInterval( moveball, 10 );
@@ -29,6 +54,12 @@ function init() {
 function moveball() {
 	ctx.clearRect( boxx, boxy, boxwidth, boxheight );
 	moveandcheck();
+
+	ctx.drawImage( img, ballx - ballrad, bally - ballrad, 2 * ballrad, 2 * ballrad );
+	ctx.fillRect( boxx, boxy, ballrad, boxheight );
+	ctx.fillRect( boxx + boxwidth - ballrad, boxy, ballrad, boxheight );
+	ctx.fillRect( boxx, boxy, boxwidth, ballrad );
+	ctx.fillRect( boxx, boxy + boxheight - ballrad, boxwidth, ballrad );
 
 	ctx.beginPath();
 	ctx.arc( ballx, bally, ballrad, 0, Math.PI * 2, true );
